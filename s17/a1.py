b6b005d325518b6443d12054579a5c82cd1d0071
islandev300 <00001103826840sp@al.educacao.sp.gov.br>
2025-08-08 08:43:24 -0300

    atividade s17 aula 1

--- s17/a1.py
+++ s17/a1.py
@@ -1,1 +1,18 @@
-print("ola")
+cargo = input("digite seu cargo: ")
+dia_da_semana = input("digite o dia da semana: ")
+   
+
+if cargo == 'gerente':
+    print("aceso liberado todos os dias")
+elif cargo == 'analista':
+    if dia_da_semana in ["segunda", "terca", "quarta", "quinta", "sexta"]:
+        dia_da_semana = ("segunda", "terca", "quarta", "quinta", "sexta")
+        print("acesso_liberado")
+    else:
+        print("acesso_negado")
+elif cargo == 'estagiario':
+    if dia_da_semana in ["segunda", "terca", "quarta", "quinta", "sexta"]:
+        dia_da_semana = ("segunda", "terca", "quarta", "quinta", "sexta")
+        print("acesso_liberado")
+    else:
+        print("acesso_negado")
